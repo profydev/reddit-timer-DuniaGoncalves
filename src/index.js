a3ce5da87ecad434abc546bbbfc26e698dc69ac1
Dunia Goncalves <dgoncalves@everquote.com>
2022-03-29 18:21:44 -0400

    delete files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './GlobalStyle';
+import theme from './theme';
+
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <h1>Hello</h1>
+  </ThemeProvider>
+);
 
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(
