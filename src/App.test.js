980c827a4c2e607e505ba012056e40ca46903f81
Dunia Goncalves <dgoncalves@everquote.com>
2022-04-03 02:02:32 -0400

    add tests to Header

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,26 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { BrowserRouter as Router} from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
+import Header from './components/Header';
 
 test('renders App', () => {
   render(<App />);
+});
+
+describe('Header', () => {
+  test('links points to the correct page', () => {
+    render(
+      <Router>
+        <Header />
+      </Router>
+    );
+    const searchLink = screen.getByRole('link', { name: /search/i });
+    const hiwLink = screen.getByRole('link', { name: /how it works/i });
+    const aboutLink = screen.getByRole('link', { name: /about/i });
+    userEvent.click(searchLink);
+    userEvent.click(hiwLink);
+    userEvent.click(aboutLink);
+  });
 });
